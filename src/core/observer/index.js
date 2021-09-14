31fcd8fca2a26b008fcc6e7a626deb2da754a135
link <cyd551646@sina.com>
2021-09-14 17:27:41 +0800

    观察者类的基本认识

--- src/core/observer/index.js
+++ src/core/observer/index.js
@@ -31,6 +31,9 @@ export const observerState = {
  * object. Once attached, the observer converts target
  * object's property keys into getter/setters that
  * collect dependencies and dispatches updates.
+ * 观察者类是一个附加于每一个被观察的对象上， 一旦被附加，
+ * 一旦被观察， 观察者将转变对象的属性的值变为一个 getter/setters
+ * 用于收集依赖 和 dispatches updates（分发更新?，不知道怎么翻译）
  */
 export class Observer {
   value: any;
@@ -38,25 +41,40 @@ export class Observer {
   vmCount: number; // number of vms that has this object as root $data
 
   constructor (value: any) {
+    /**
+     * @param {*} value // 被观测对象
+     *
+    */
     this.value = value
     this.dep = new Dep()
     this.vmCount = 0
+    // 给当前的被观测对象定义一个属性， 该属性为一个 观察者实例
+    // 相当于给当前的被观测对象打上一个标签， 避免重复操作
     def(value, '__ob__', this)
     if (Array.isArray(value)) {
+      // 当前被观测对象如果是一个 数组
       const augment = hasProto
         ? protoAugment
         : copyAugment
       augment(value, arrayMethods, arrayKeys)
       this.observeArray(value)
+
     } else {
+      // 对象
       this.walk(value)
     }
   }
 
   /**
+
+   * @description:
    * Walk through each property and convert them into
    * getter/setters. This method should only be called when
    * value type is Object.
+   * walk 函数将对象每个属性转变为 getter/setters 的形式，
+   * 并且这个函数仅在 参数类型 为对象的时候触发
+   * @param {*} obj 被观测的目标对象
+   * @return {*}
    */
   walk (obj: Object) {
     const keys = Object.keys(obj)
@@ -100,24 +118,36 @@ function copyAugment (target: Object, src: Object, keys: Array<string>) {
 }
 
 /**
+ * @description: 
  * Attempt to create an observer instance for a value,
  * returns the new observer if successfully observed,
  * or the existing observer if the value already has one.
+ * 尝试为当前的值创建一个观察者实例，
+ * 如果能成功被观察， 则返回一个新的观察者实例
+ * 或者返回一个已经存在的观察者，如果该值已经有了
+ * @param {*} value 目标值
+ * @param {*} asRootData
+ * @return {*}
  */
 export function observe (value: any, asRootData: ?boolean): Observer | void {
+  // 不是一个对象
   if (!isObject(value) || value instanceof VNode) {
     return
   }
   let ob: Observer | void
+  // 存在观察者实例， 返回旧的
   if (hasOwn(value, '__ob__') && value.__ob__ instanceof Observer) {
     ob = value.__ob__
   } else if (
     observerState.shouldConvert &&
     !isServerRendering() &&
+    // 数组， 扁平对象
     (Array.isArray(value) || isPlainObject(value)) &&
+    // 目标是否可扩展
     Object.isExtensible(value) &&
     !value._isVue
   ) {
+    // 返回新观察者实例
     ob = new Observer(value)
   }
   if (asRootData && ob) {
@@ -127,7 +157,16 @@ export function observe (value: any, asRootData: ?boolean): Observer | void {
 }
 
 /**
+ * @description:
  * Define a reactive property on an Object.
+ * 在一个对象上定义一个响应式属性
+ * @param {*} obj 目标对象
+ * @param {*} key 目标对象的键
+ * @param {*} val 对应键的值
+ * @param {*} customSetter
+ * @param {*} shallow 是否为扁平的对象
+ * @param {*}
+ * @return {*}
  */
 export function defineReactive (
   obj: Object,
@@ -137,8 +176,8 @@ export function defineReactive (
   shallow?: boolean
 ) {
   const dep = new Dep()
-
   const property = Object.getOwnPropertyDescriptor(obj, key)
+  // 当前属性私有属性， 但不可设置
   if (property && property.configurable === false) {
     return
   }
@@ -146,8 +185,9 @@ export function defineReactive (
   // cater for pre-defined getter/setters
   const getter = property && property.get
   const setter = property && property.set
-
+  // 递归，拿到属性的观察者对象
   let childOb = !shallow && observe(val)
+  
   Object.defineProperty(obj, key, {
     enumerable: true,
     configurable: true,
--- src/core/util/lang.js
+++ src/core/util/lang.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-09-13 11:39:57
+ * @LastEditTime: 2021-09-14 16:21:49
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \vue-core\src\core\util\lang.js
+ */
 /* @flow */
 
 export const emptyObject = Object.freeze({})
@@ -9,9 +17,13 @@ export function isReserved (str: string): boolean {
   const c = (str + '').charCodeAt(0)
   return c === 0x24 || c === 0x5F
 }
-
 /**
- * Define a property.
+ * @description:
+ * @param {*} obj 添加属性的目标对象
+ * @param {*} key 键
+ * @param {*} val 值
+ * @param {*} enumerable 是否可枚举
+ * @return {*}
  */
 export function def (obj: Object, key: string, val: any, enumerable?: boolean) {
   Object.defineProperty(obj, key, {

710cc3b2389ae9bc2923d5288c471ab6f6e6f6a3
Link <cyd551646@sina.com>
2022-01-13 10:02:40 +0800

    release:

---
+++ scripts/release.js
@@ -0,0 +1,49 @@
+/**
+ * 1. 一键push 并带有提交信息
+ * 2. 自动增加日志
+ */
+const args = require('minimist')(process.argv.slice(2))
+const chalk = require('chalk')
+const execa = require('execa')
+
+const isDryRun = args.dry
+
+const run = (bin, args, opts = {}) =>
+  execa(bin, args, { stdio: 'inherit', ...opts })
+const dryRun = (bin, args, opts = {}) =>
+  console.log(chalk.blue(`[dryrun] ${bin} ${args.join(' ')}`), opts)
+const runIfNotDry = isDryRun ? dryRun : run
+
+const step = msg => console.log(chalk.cyan(msg))
+
+
+async function main () {
+
+  // await run(`yarn`, ['changelog'])
+  console.log(args)
+
+  const { stdout } = await run('git', ['diff'], { stdio: 'pipe' })
+
+  if (stdout) {
+    step('\n Runing push code')
+    await runIfNotDry('git', ['add', '-A'])
+    await runIfNotDry('git', ['commit', '-m', `release: `])
+
+    const { yes } = await prompt({
+      type: 'confirm',
+      name: 'yes',
+      message: `Are you sure to push?`
+    })
+
+    if (!yes) return
+
+    await runIfNotDry('git', ['push'])
+
+  }
+}
+
+main().catch(error => {
+  console.error(error)
+})
+
+// npm run link "feat: finish reactive" --dry
